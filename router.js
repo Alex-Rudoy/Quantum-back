21b3c210f8b7206155b8628785bdb0a61b5b7c4d
Alex Rudoy <rudoy4ik@gmail.com>
2022-08-24 22:45:03 +0300

    init

---
+++ router.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const userController = require("./controllers/feedbackController");
+const stepController = require("./controllers/stepController");
+const cors = require("cors");
+
+router.use(cors());
+
+router.get("/", (req, res) =>
+  res.json(
+    "Hello, if you see this message that means your backend is up and running successfully."
+  )
+);
+
+// check token to log out front-end if expired
+router.post("/checkToken", userController.checkToken);
+
+module.exports = router;
